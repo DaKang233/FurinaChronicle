10670bf75b8ee5361ef4ef777a91e53b12fd10f8
DaKang233 <100811260+DaKang233@users.noreply.github.com>
2026-07-19 20:06:48 +0800

    feat: add player archive and game account models

---
+++ FurinaArchive.Core/Archives/GameAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurinaArchive.Core.Archives
+{
+    public sealed record GameAccount(
+        Guid Id, 
+        Guid PlayerArchiveId, string Uid, 
+        GameServerRegion ServerRegion, 
+        string? DisplayName, 
+        bool IsPlaceholder, 
+        DateTimeOffset CreatedAt, 
+        DateTimeOffset UpdatedAt);
+}
---
+++ FurinaArchive.Core/Archives/GameServerRegion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurinaArchive.Core.Archives
+{
+    public enum GameServerRegion
+    {
+        Unknown = 0,
+        ChinaOfficial = 1,
+        ChinaBilibili = 2,
+        America = 3,
+        Europe = 4,
+        Asia = 5,
+        TaiwanHongKongMacao = 6,
+    }
+}
